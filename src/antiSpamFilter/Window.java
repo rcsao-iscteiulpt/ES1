ceee42f934ec653b8fe5f7ac35b31f4f97939a63
RicardoSilva <rcsao@iscte-iul.pt>
2017-12-01 23:37:47 +0000

    é so um update na interface do david e acrescentei os ficheiros ham.log,spam.log e rules.cf a uma folder chamada files

--- src/antiSpamFilter/Window.java
+++ src/antiSpamFilter/Window.java
@@ -3,24 +3,29 @@ package antiSpamFilter;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
-import javax.swing.JPanel;
-import java.awt.BorderLayout;
-import java.awt.GridLayout;
-import java.awt.FlowLayout;
-import javax.swing.JTextField;
-import javax.swing.JButton;
 import java.awt.GridBagLayout;
+import javax.swing.JPanel;
+import java.awt.Color;
 import java.awt.GridBagConstraints;
 import java.awt.Insets;
-import javax.swing.JList;
-import javax.swing.JTextPane;
+import java.awt.FlowLayout;
+import javax.swing.JSplitPane;
+import java.awt.GridLayout;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import javax.swing.JButton;
+import java.awt.Font;
+import javax.swing.JLabel;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JTextArea;
 
 public class Window {
 
 	private JFrame frame;
-	private JTextField textField1;
-	private JTextField textField2;
-	private JTextField textField3;
+	private JTextField Path_rules;
+	private JTextField Path_Spam;
+	private JTextField Path_Ham;
 
 	/**
 	 * Launch the application.
@@ -50,170 +55,269 @@ public class Window {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.getContentPane().setBackground(Color.BLACK);
+		frame.setBounds(100, 100, 997, 935);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(new GridLayout(3, 1, 0, 0));
+		GridBagLayout gridBagLayout = new GridBagLayout();
+		gridBagLayout.rowHeights = new int[] {262, 244, 0};
+		gridBagLayout.columnWidths = new int[] {3};
+		gridBagLayout.columnWeights = new double[]{1.0};
+		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 1.0};
+		frame.getContentPane().setLayout(gridBagLayout);
 		
-		JPanel panel1 = new JPanel();
-		frame.getContentPane().add(panel1);
-		panel1.setLayout(new GridLayout(3, 1, 0, 0));
+		JPanel panel_paths_files = new JPanel();
+		panel_paths_files.setBackground(Color.BLACK);
+		panel_paths_files.setForeground(Color.BLACK);
+		GridBagConstraints gbc_panel_paths_files = new GridBagConstraints();
+		gbc_panel_paths_files.insets = new Insets(0, 0, 5, 0);
+		gbc_panel_paths_files.fill = GridBagConstraints.BOTH;
+		gbc_panel_paths_files.gridx = 0;
+		gbc_panel_paths_files.gridy = 0;
+		frame.getContentPane().add(panel_paths_files, gbc_panel_paths_files);
+		GridBagLayout gbl_panel_paths_files = new GridBagLayout();
+		gbl_panel_paths_files.columnWidths = new int[]{496, 173, 308, 0};
+		gbl_panel_paths_files.rowHeights = new int[]{257, 0};
+		gbl_panel_paths_files.columnWeights = new double[]{0.0, 0.0, 0.0, Double.MIN_VALUE};
+		gbl_panel_paths_files.rowWeights = new double[]{0.0, Double.MIN_VALUE};
+		panel_paths_files.setLayout(gbl_panel_paths_files);
 		
-		JPanel panel1_1 = new JPanel();
-		panel1.add(panel1_1);
-		GridBagLayout gbl_panel1_1 = new GridBagLayout();
-		gbl_panel1_1.columnWidths = new int[]{99, 130, 117, 0};
-		gbl_panel1_1.rowHeights = new int[]{29, 0};
-		gbl_panel1_1.columnWeights = new double[]{0.0, 0.0, 0.0, Double.MIN_VALUE};
-		gbl_panel1_1.rowWeights = new double[]{0.0, Double.MIN_VALUE};
-		panel1_1.setLayout(gbl_panel1_1);
+		JPanel panel_paths = new JPanel();
+		panel_paths.setBackground(Color.BLACK);
+		GridBagConstraints gbc_panel_paths = new GridBagConstraints();
+		gbc_panel_paths.fill = GridBagConstraints.BOTH;
+		gbc_panel_paths.insets = new Insets(0, 0, 0, 5);
+		gbc_panel_paths.gridx = 0;
+		gbc_panel_paths.gridy = 0;
+		panel_paths_files.add(panel_paths, gbc_panel_paths);
+		panel_paths.setLayout(new GridLayout(3, 0, 1, 20));
 		
-		textField1 = new JTextField();
-		GridBagConstraints gbc_textField1 = new GridBagConstraints();
-		gbc_textField1.anchor = GridBagConstraints.WEST;
-		gbc_textField1.insets = new Insets(0, 0, 0, 5);
-		gbc_textField1.gridx = 1;
-		gbc_textField1.gridy = 0;
-		panel1_1.add(textField1, gbc_textField1);
-		textField1.setColumns(10);
+		Path_rules = new JTextField();
+		Path_rules.setEditable(false);
+		Path_rules.setFont(new Font("Tahoma", Font.PLAIN, 22));
+		Path_rules.setHorizontalAlignment(SwingConstants.CENTER);
+		Path_rules.setText("adasda");
+		panel_paths.add(Path_rules);
+		Path_rules.setColumns(10);
 		
-		JButton Rules_cf_Button = new JButton("Rules.cf");
-		GridBagConstraints gbc_Rules_cf_Button = new GridBagConstraints();
-		gbc_Rules_cf_Button.anchor = GridBagConstraints.NORTHWEST;
-		gbc_Rules_cf_Button.gridx = 2;
-		gbc_Rules_cf_Button.gridy = 0;
-		panel1_1.add(Rules_cf_Button, gbc_Rules_cf_Button);
+		Path_Spam = new JTextField();
+		Path_Spam.setEditable(false);
+		panel_paths.add(Path_Spam);
+		Path_Spam.setColumns(10);
 		
-		JPanel panel1_2 = new JPanel();
-		panel1.add(panel1_2);
-		GridBagLayout gbl_panel1_2 = new GridBagLayout();
-		gbl_panel1_2.columnWidths = new int[]{99, 130, 117, 0};
-		gbl_panel1_2.rowHeights = new int[]{29, 0};
-		gbl_panel1_2.columnWeights = new double[]{0.0, 0.0, 0.0, Double.MIN_VALUE};
-		gbl_panel1_2.rowWeights = new double[]{0.0, Double.MIN_VALUE};
-		panel1_2.setLayout(gbl_panel1_2);
+		Path_Ham = new JTextField();
+		Path_Ham.setEditable(false);
+		panel_paths.add(Path_Ham);
+		Path_Ham.setColumns(10);
 		
-		textField2 = new JTextField();
-		textField2.setColumns(10);
-		GridBagConstraints gbc_textField2 = new GridBagConstraints();
-		gbc_textField2.anchor = GridBagConstraints.WEST;
-		gbc_textField2.insets = new Insets(0, 0, 0, 5);
-		gbc_textField2.gridx = 1;
-		gbc_textField2.gridy = 0;
-		panel1_2.add(textField2, gbc_textField2);
+		JPanel panel_change_buttons = new JPanel();
+		panel_change_buttons.setBackground(Color.BLACK);
+		GridBagConstraints gbc_panel_change_buttons = new GridBagConstraints();
+		gbc_panel_change_buttons.fill = GridBagConstraints.BOTH;
+		gbc_panel_change_buttons.insets = new Insets(0, 0, 0, 5);
+		gbc_panel_change_buttons.gridx = 1;
+		gbc_panel_change_buttons.gridy = 0;
+		panel_paths_files.add(panel_change_buttons, gbc_panel_change_buttons);
+		panel_change_buttons.setLayout(new GridLayout(3, 0, 0, 20));
 		
-		JButton Spam_log_Button = new JButton("SPAM.log");
-		GridBagConstraints gbc_Spam_log_Button = new GridBagConstraints();
-		gbc_Spam_log_Button.anchor = GridBagConstraints.NORTHWEST;
-		gbc_Spam_log_Button.gridx = 2;
-		gbc_Spam_log_Button.gridy = 0;
-		panel1_2.add(Spam_log_Button, gbc_Spam_log_Button);
+		JButton Change_Path_Rules = new JButton("Change Path");
+		Change_Path_Rules.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
 		
-		JPanel panel1_3 = new JPanel();
-		panel1.add(panel1_3);
-		GridBagLayout gbl_panel1_3 = new GridBagLayout();
-		gbl_panel1_3.columnWidths = new int[]{99, 130, 117, 0};
-		gbl_panel1_3.rowHeights = new int[]{29, 0, 0};
-		gbl_panel1_3.columnWeights = new double[]{1.0, 0.0, 0.0, Double.MIN_VALUE};
-		gbl_panel1_3.rowWeights = new double[]{0.0, 1.0, Double.MIN_VALUE};
-		panel1_3.setLayout(gbl_panel1_3);
 		
-		textField3 = new JTextField();
-		textField3.setColumns(10);
-		GridBagConstraints gbc_textField3 = new GridBagConstraints();
-		gbc_textField3.anchor = GridBagConstraints.WEST;
-		gbc_textField3.insets = new Insets(0, 0, 5, 5);
-		gbc_textField3.gridx = 1;
-		gbc_textField3.gridy = 0;
-		panel1_3.add(textField3, gbc_textField3);
+		Change_Path_Rules.setBackground(Color.BLACK);
+		Change_Path_Rules.setForeground(Color.BLACK);
+		panel_change_buttons.add(Change_Path_Rules);
 		
-		JButton Ham_log_Button = new JButton("Ham.log");
-		GridBagConstraints gbc_Ham_log_Button = new GridBagConstraints();
-		gbc_Ham_log_Button.insets = new Insets(0, 0, 5, 0);
-		gbc_Ham_log_Button.anchor = GridBagConstraints.NORTHWEST;
-		gbc_Ham_log_Button.gridx = 2;
-		gbc_Ham_log_Button.gridy = 0;
-		panel1_3.add(Ham_log_Button, gbc_Ham_log_Button);
+		JButton Change_Path_Spam = new JButton("Change Path");
+		panel_change_buttons.add(Change_Path_Spam);
 		
-		JPanel panel2 = new JPanel();
-		frame.getContentPane().add(panel2);
-		panel2.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		JButton Change_Path_Ham = new JButton("Change Path");
+		panel_change_buttons.add(Change_Path_Ham);
 		
-		JPanel panel2_1 = new JPanel();
-		panel2.add(panel2_1);
-		panel2_1.setLayout(new GridLayout(2, 1, 0, 0));
+		JPanel panel_files_names = new JPanel();
+		panel_files_names.setBackground(Color.BLACK);
+		GridBagConstraints gbc_panel_files_names = new GridBagConstraints();
+		gbc_panel_files_names.fill = GridBagConstraints.BOTH;
+		gbc_panel_files_names.gridx = 2;
+		gbc_panel_files_names.gridy = 0;
+		panel_paths_files.add(panel_files_names, gbc_panel_files_names);
+		panel_files_names.setLayout(new GridLayout(0, 1, 0, 20));
 		
-		JPanel panel2_1_1 = new JPanel();
-		panel2_1.add(panel2_1_1);
-		panel2_1_1.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		JLabel file1 = new JLabel("Rules.cf");
+		file1.setFont(new Font("Tahoma", Font.PLAIN, 30));
+		file1.setHorizontalAlignment(SwingConstants.CENTER);
+		file1.setForeground(Color.WHITE);
+		panel_files_names.add(file1);
 		
-		JList list1 = new JList();
-		panel2_1_1.add(list1);
+		JLabel file2 = new JLabel("Spam.log");
+		file2.setBackground(Color.PINK);
+		file2.setFont(new Font("Tahoma", Font.PLAIN, 30));
+		file2.setForeground(Color.WHITE);
+		file2.setHorizontalAlignment(SwingConstants.CENTER);
+		panel_files_names.add(file2);
 		
-		JList list2 = new JList();
-		panel2_1_1.add(list2);
+		JLabel file3 = new JLabel("Ham.log");
+		file3.setHorizontalAlignment(SwingConstants.CENTER);
+		file3.setForeground(Color.WHITE);
+		file3.setFont(new Font("Tahoma", Font.PLAIN, 30));
+		panel_files_names.add(file3);
 		
-		JPanel panel2_1_2 = new JPanel();
-		panel2_1.add(panel2_1_2);
-		panel2_1_2.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		JPanel panel_manual_conf = new JPanel();
+		panel_manual_conf.setBackground(Color.BLACK);
+		GridBagConstraints gbc_panel_manual_conf = new GridBagConstraints();
+		gbc_panel_manual_conf.insets = new Insets(0, 0, 5, 0);
+		gbc_panel_manual_conf.fill = GridBagConstraints.BOTH;
+		gbc_panel_manual_conf.gridx = 0;
+		gbc_panel_manual_conf.gridy = 1;
+		frame.getContentPane().add(panel_manual_conf, gbc_panel_manual_conf);
+		GridBagLayout gbl_panel_manual_conf = new GridBagLayout();
+		gbl_panel_manual_conf.columnWidths = new int[]{681, 295, 0};
+		gbl_panel_manual_conf.rowHeights = new int[]{239, 0};
+		gbl_panel_manual_conf.columnWeights = new double[]{0.0, 0.0, Double.MIN_VALUE};
+		gbl_panel_manual_conf.rowWeights = new double[]{0.0, Double.MIN_VALUE};
+		panel_manual_conf.setLayout(gbl_panel_manual_conf);
 		
-		JTextPane txtpnFp = new JTextPane();
-		txtpnFp.setText("FP:");
-		txtpnFp.setEditable(false);
-		panel2_1_2.add(txtpnFp);
+		JPanel panel_left = new JPanel();
+		GridBagConstraints gbc_panel_left = new GridBagConstraints();
+		gbc_panel_left.fill = GridBagConstraints.BOTH;
+		gbc_panel_left.insets = new Insets(0, 0, 0, 5);
+		gbc_panel_left.gridx = 0;
+		gbc_panel_left.gridy = 0;
+		panel_manual_conf.add(panel_left, gbc_panel_left);
+		GridBagLayout gbl_panel_left = new GridBagLayout();
+		gbl_panel_left.columnWidths = new int[]{543, 132, 0};
+		gbl_panel_left.rowHeights = new int[]{195, 59, 0};
+		gbl_panel_left.columnWeights = new double[]{0.0, 0.0, Double.MIN_VALUE};
+		gbl_panel_left.rowWeights = new double[]{0.0, 0.0, Double.MIN_VALUE};
+		panel_left.setLayout(gbl_panel_left);
 		
-		JTextPane txtpnFn = new JTextPane();
-		txtpnFn.setText("FN:");
-		panel2_1_2.add(txtpnFn);
+		JTextArea list_rules_manual = new JTextArea();
+		GridBagConstraints gbc_list_rules_manual = new GridBagConstraints();
+		gbc_list_rules_manual.fill = GridBagConstraints.BOTH;
+		gbc_list_rules_manual.insets = new Insets(0, 0, 5, 5);
+		gbc_list_rules_manual.gridx = 0;
+		gbc_list_rules_manual.gridy = 0;
+		panel_left.add(list_rules_manual, gbc_list_rules_manual);
 		
-		JPanel panel2_2 = new JPanel();
-		panel2.add(panel2_2);
-		panel2_2.setLayout(new GridLayout(0, 1, 0, 0));
+		JTextArea rules_weight_manual = new JTextArea();
+		GridBagConstraints gbc_rules_weight_manual = new GridBagConstraints();
+		gbc_rules_weight_manual.fill = GridBagConstraints.BOTH;
+		gbc_rules_weight_manual.insets = new Insets(0, 0, 5, 0);
+		gbc_rules_weight_manual.gridx = 1;
+		gbc_rules_weight_manual.gridy = 0;
+		panel_left.add(rules_weight_manual, gbc_rules_weight_manual);
 		
-		JButton avaliacao_qualidade_conf_button = new JButton("Avaliaçao Qualidade Conf");
-		panel2_2.add(avaliacao_qualidade_conf_button);
+		JLabel label_results_manual = new JLabel("FP:           FN:");
+		label_results_manual.setFont(new Font("Tahoma", Font.PLAIN, 30));
+		label_results_manual.setHorizontalAlignment(SwingConstants.CENTER);
+		GridBagConstraints gbc_label_results_manual = new GridBagConstraints();
+		gbc_label_results_manual.fill = GridBagConstraints.BOTH;
+		gbc_label_results_manual.insets = new Insets(0, 0, 0, 5);
+		gbc_label_results_manual.gridx = 0;
+		gbc_label_results_manual.gridy = 1;
+		panel_left.add(label_results_manual, gbc_label_results_manual);
 		
-		JButton gravar_conf_button = new JButton("Gravar Configuraçao");
-		panel2_2.add(gravar_conf_button);
+		JPanel panel_right = new JPanel();
+		GridBagConstraints gbc_panel_right = new GridBagConstraints();
+		gbc_panel_right.fill = GridBagConstraints.BOTH;
+		gbc_panel_right.gridx = 1;
+		gbc_panel_right.gridy = 0;
+		panel_manual_conf.add(panel_right, gbc_panel_right);
+		panel_right.setLayout(new GridLayout(0, 1, 0, 0));
 		
-		JPanel panel3 = new JPanel();
-		frame.getContentPane().add(panel3);
+		JButton button_avaliar = new JButton("Avaliar Configuracao Manual");
+		button_avaliar.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		panel_right.add(button_avaliar);
 		
-		JPanel panel3_1 = new JPanel();
-		panel3.add(panel3_1);
-		panel3_1.setLayout(new GridLayout(2, 1, 0, 0));
+		JButton button_gravar_manual = new JButton("Gravar Configuracao Manual");
+		button_gravar_manual.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		panel_right.add(button_gravar_manual);
 		
-		JPanel panel3_1_1 = new JPanel();
-		panel3_1.add(panel3_1_1);
-		panel3_1_1.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		JPanel panel_auto_conf = new JPanel();
+		GridBagConstraints gbc_panel_auto_conf = new GridBagConstraints();
+		gbc_panel_auto_conf.fill = GridBagConstraints.BOTH;
+		gbc_panel_auto_conf.gridx = 0;
+		gbc_panel_auto_conf.gridy = 2;
+		frame.getContentPane().add(panel_auto_conf, gbc_panel_auto_conf);
+		GridBagLayout gbl_panel_auto_conf = new GridBagLayout();
+		gbl_panel_auto_conf.columnWidths = new int[]{972, 0};
+		gbl_panel_auto_conf.rowHeights = new int[]{157, 157, 0};
+		gbl_panel_auto_conf.columnWeights = new double[]{0.0, Double.MIN_VALUE};
+		gbl_panel_auto_conf.rowWeights = new double[]{0.0, 0.0, Double.MIN_VALUE};
+		panel_auto_conf.setLayout(gbl_panel_auto_conf);
 		
-		JList list = new JList();
-		panel3_1_1.add(list);
+		JPanel panel_up = new JPanel();
+		GridBagConstraints gbc_panel_up = new GridBagConstraints();
+		gbc_panel_up.fill = GridBagConstraints.BOTH;
+		gbc_panel_up.insets = new Insets(0, 0, 5, 0);
+		gbc_panel_up.gridx = 0;
+		gbc_panel_up.gridy = 0;
+		panel_auto_conf.add(panel_up, gbc_panel_up);
+		GridBagLayout gbl_panel_up = new GridBagLayout();
+		gbl_panel_up.columnWidths = new int[]{816, 158, 0};
+		gbl_panel_up.rowHeights = new int[]{146, 62, 0};
+		gbl_panel_up.columnWeights = new double[]{0.0, 0.0, Double.MIN_VALUE};
+		gbl_panel_up.rowWeights = new double[]{0.0, 0.0, Double.MIN_VALUE};
+		panel_up.setLayout(gbl_panel_up);
 		
-		JList list_1 = new JList();
-		panel3_1_1.add(list_1);
+		JTextArea list_rules_auto = new JTextArea();
+		GridBagConstraints gbc_list_rules_auto = new GridBagConstraints();
+		gbc_list_rules_auto.fill = GridBagConstraints.BOTH;
+		gbc_list_rules_auto.insets = new Insets(0, 0, 5, 5);
+		gbc_list_rules_auto.gridx = 0;
+		gbc_list_rules_auto.gridy = 0;
+		panel_up.add(list_rules_auto, gbc_list_rules_auto);
 		
-		JPanel panel3_1_2 = new JPanel();
-		panel3_1.add(panel3_1_2);
-		panel3_1_2.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		JTextArea rules_weight_auto = new JTextArea();
+		GridBagConstraints gbc_rules_weight_auto = new GridBagConstraints();
+		gbc_rules_weight_auto.fill = GridBagConstraints.BOTH;
+		gbc_rules_weight_auto.insets = new Insets(0, 0, 5, 0);
+		gbc_rules_weight_auto.gridx = 1;
+		gbc_rules_weight_auto.gridy = 0;
+		panel_up.add(rules_weight_auto, gbc_rules_weight_auto);
 		
-		JTextPane textPane = new JTextPane();
-		textPane.setText("FP:");
-		textPane.setEditable(false);
-		panel3_1_2.add(textPane);
+		JLabel label_results_auto = new JLabel("FP:           FN:");
+		label_results_auto.setHorizontalAlignment(SwingConstants.CENTER);
+		GridBagConstraints gbc_label_results_auto = new GridBagConstraints();
+		gbc_label_results_auto.fill = GridBagConstraints.BOTH;
+		gbc_label_results_auto.insets = new Insets(0, 0, 0, 5);
+		gbc_label_results_auto.gridx = 0;
+		gbc_label_results_auto.gridy = 1;
+		panel_up.add(label_results_auto, gbc_label_results_auto);
+		label_results_auto.setFont(new Font("Tahoma", Font.PLAIN, 30));
 		
-		JTextPane textPane_1 = new JTextPane();
-		textPane_1.setText("FN:");
-		panel3_1_2.add(textPane_1);
+		JPanel panel_down = new JPanel();
+		GridBagConstraints gbc_panel_down = new GridBagConstraints();
+		gbc_panel_down.fill = GridBagConstraints.BOTH;
+		gbc_panel_down.gridx = 0;
+		gbc_panel_down.gridy = 1;
+		panel_auto_conf.add(panel_down, gbc_panel_down);
+		GridBagLayout gbl_panel_down = new GridBagLayout();
+		gbl_panel_down.columnWidths = new int[]{973, 0};
+		gbl_panel_down.rowHeights = new int[]{78, 71, 0};
+		gbl_panel_down.columnWeights = new double[]{0.0, Double.MIN_VALUE};
+		gbl_panel_down.rowWeights = new double[]{0.0, 0.0, Double.MIN_VALUE};
+		panel_down.setLayout(gbl_panel_down);
 		
-		JPanel panel3_2 = new JPanel();
-		panel3.add(panel3_2);
-		panel3_2.setLayout(new GridLayout(0, 1, 0, 0));
+		JButton button_gerar_conf = new JButton("Gerar Configuracao Autom\u00E1tica");
+		GridBagConstraints gbc_button_gerar_conf = new GridBagConstraints();
+		gbc_button_gerar_conf.fill = GridBagConstraints.BOTH;
+		gbc_button_gerar_conf.insets = new Insets(0, 0, 5, 0);
+		gbc_button_gerar_conf.gridx = 0;
+		gbc_button_gerar_conf.gridy = 0;
+		panel_down.add(button_gerar_conf, gbc_button_gerar_conf);
+		button_gerar_conf.setFont(new Font("Tahoma", Font.PLAIN, 20));
 		
-		JButton button = new JButton("Gerar Conf Automatica");
-		panel3_2.add(button);
-		
-		JButton button_1 = new JButton("Gravar Conf");
-		panel3_2.add(button_1);
+		JButton button_gravar_auto = new JButton("Gravar Configuracao Autom\u00E1tica");
+		GridBagConstraints gbc_button_gravar_auto = new GridBagConstraints();
+		gbc_button_gravar_auto.fill = GridBagConstraints.BOTH;
+		gbc_button_gravar_auto.gridx = 0;
+		gbc_button_gravar_auto.gridy = 1;
+		panel_down.add(button_gravar_auto, gbc_button_gravar_auto);
+		button_gravar_auto.setFont(new Font("Tahoma", Font.PLAIN, 20));
 	}
+
 
 }
